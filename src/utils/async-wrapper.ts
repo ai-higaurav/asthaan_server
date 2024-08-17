cad04b4086e7ce032e4b898cf7f5587dce75b688
hydra <ai.higaurav@gmail.com>
2024-08-17 09:51:47 +0530

    async wrapper added

---
+++ src/utils/async-wrapper.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+
+type AsyncFn = (req: Request, res: Response, next: NextFunction) => Promise<any>
+
+const asyncHandler = async (fn: AsyncFn) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await fn(req, res, next)
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+export default asyncHandler;
